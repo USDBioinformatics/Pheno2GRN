c0c7b21b538b0ee281cd8c6519513b902d0338ac
ruger747 <nick.weinandt@gmail.com>
2014-08-12 09:47:48 -0500

    Added documentation.

--- src/java/edu/usd/pheno2grn/iplant/IplantCredentials.java
+++ src/java/edu/usd/pheno2grn/iplant/IplantCredentials.java
@@ -2,11 +2,9 @@ package edu.usd.pheno2grn.iplant;
 
 /**
  * This class is used to store the username and password for Iplant. Please
- * enter your own user and password here
- * 
- * @author Nick.Weinandt
+ * enter your own user and password here.
  */
 public class IplantCredentials {
-    public static final String USERNAME="usdbioinformatics";
-    public static final String PASSWORD="U3k4u5a6";
+    public static final String USERNAME="";
+    public static final String PASSWORD="";
 }
